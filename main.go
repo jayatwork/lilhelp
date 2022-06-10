2ed4a9bcbd2f3a7a4d988a7b406806e1f9807685
Jason Ancrum <jason.ancrum00@delta.com>
2022-06-10 17:05:28 -0400

    added another attempt at array selection struct

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ func main() {
 	//initialize and prep the commandline flags
 	f.Init()
 	//Iterate through brower collections depending on flag selected
+	
 	b.Openbrowser("https://www16.v1host.com/Delta_Air_Lines/TeamRoom.mvc/Show/3036441") //open browser with native cmd
 
 }
--- startup/cmdline/flags.go
+++ startup/cmdline/flags.go
@@ -3,18 +3,56 @@ package cmdline
 import (
 	"flag"
 	"fmt"
+	"log"
+	"os/exec"
 )
 
-func Init() {
-	all := flag.String("all", "foo", "Invoker help util across ALL projects")
-	ccoe := flag.String("ccoe", "foo", "Invoker help util across CCOE projects")
-	paas := flag.String("paas", "foo", "Invoker help util across PAAS projects")
-	devops := flag.String("devops", "foo", "Invoker help util across PAAS projects")
+type selection struct {
+	address string
+}
+
+func Init() []selection {
+	fmt.Println("\n\nUsage: \nlilhelp utility usage guide")
+	all := flag.String("all", "-all", "Invoker help util across ALL projects")
+	ccoe := flag.String("ccoe", "-ccoe", "Invoker help util across CCOE projects")
+	paas := flag.String("paas", "-paas", "Invoker help util across PAAS projects")
+	devops := flag.String("devops", "-devops", "Invoker help util across PAAS projects")
 
 	flag.Parse()
 
+	input := []selection{""}
 	fmt.Println("All helper:", *all)
 	fmt.Println("CCOE helper:", *ccoe)
 	fmt.Println("PAAS helper:", *paas)
 	fmt.Println("DEVOPS helper:", *devops)
+
+	// TODO follow on the array of selection type struct
+	basenameOpts = []opt{
+		opt{
+			shortnm: 'a',
+			longnm:  "multiple",
+			needArg: false,
+			help:    "Usage for a",
+		},
+		opt{
+			shortnm: 'b',
+			longnm:  "b-option",
+			needArg: false,
+			help:    "Usage for b",
+		},
+	}
+
+	switch flag.Getter.Get() {
+	case "all":
+		return []selection{"url": "somestring"}
+	case "ccoe":
+		err = exec.Command("rundll32", "url.dll,FileProtocolHandler", url).Start()
+	case "paas":
+		err = exec.Command("open", url).Start()
+	default:
+		err = fmt.Errorf("unsupported helper domain selected")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
 }
